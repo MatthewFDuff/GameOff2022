e4a966e87196250653eb4e7f3db49312fe8c064c
Brad Oosterbroek <bradoosterbroek@gmail.com>
2022-11-03 23:26:57 -0400

    Added a Controls SO
    New controls can be added to the SO for reference and as a central place to save and load them.

---
+++ GameOff2022/Assets/Core/Scripts/Controls.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Core.Scripts
+{
+    [CreateAssetMenu(fileName = "Controls", menuName = "Scriptables/Controls", order = 0)]
+    public class Controls : ScriptableObject
+    {
+        // As we create controls, assign them here with a default.
+        public KeyCode attack;
+        public KeyCode jump = KeyCode.Space;
+
+        public void Save()
+        {
+            PlayerPrefs.GetInt("attack", (int) attack);
+            PlayerPrefs.GetInt("jump", (int) jump);
+            PlayerPrefs.Save();
+        }
+
+        public void Load()
+        {
+            attack = (KeyCode) PlayerPrefs.GetInt("attack", (int) attack);
+            jump = (KeyCode) PlayerPrefs.GetInt("jump", (int) jump);
+        }
+    }
+}
