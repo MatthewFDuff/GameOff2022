7d86565cd220058ee30b88401f36dc94546f0ba7
Matthew Duff <matthewfduff@gmail.com>
2022-11-04 21:42:28 -0400

    Added Utility Scripts
    [Singleton] Used to make a component/GameObject a singleton.
    [Persistent Singleton] Used to make a component/GameObject a singleton that persists between scenes (DontDestroyOnLoad).

---
+++ GameOff2022/Assets/Core/Scripts/Utility/Singletons/PersistentSingleton.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace Core.Scripts.Utility.Singletons
+{
+    /// <summary>
+    /// A singleton that does not get destroyed when the scene changes.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class PersistentSingleton<T> : MonoBehaviour where T : Component
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance != null) return _instance;
+
+                _instance = FindObjectOfType<T>();
+
+                if (_instance != null) return _instance;
+                
+                var newInstance = new GameObject();
+                _instance = newInstance.AddComponent<T>();
+
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// When a new instance is created we check if there's already an instance of the object. If there is one, we destroy the new one.
+        /// </summary>
+        protected virtual void Awake()
+        {
+            if (!Application.isPlaying) return;
+
+            if (_instance == null)
+            {
+                _instance = this as T;
+                DontDestroyOnLoad(transform.gameObject);
+            }
+            else
+            {
+                if(this != _instance) Destroy(gameObject);
+            }
+        }
+    }
+}
