b97310ce852b795b4a2eae992685a7d667f264a8
Eriki Enos <Erikjenos@hotmail.com>
2022-11-13 13:25:35 -0800

    Customer Spawn
    Spawning Customer. moving them to the window

---
+++ GameOff2022/Assets/Core/Modules/Battle/CustomerSpawner.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Core.Modules.Battle
+{
+    public class CustomerSpawner : MonoBehaviour
+    {
+        public Customer customerObj;
+
+        public List<GameObject> Line;
+
+        public Transform target;
+
+        private float speed = 1.0f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Line.Add(Instantiate(customerObj.customerGO, gameObject.transform.position, Quaternion.identity));
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            MoveCustomer();
+        }
+
+
+
+        private void MoveCustomer()
+        {
+            for(int x = 0; x < Line.Count; x++)
+            {
+                if(Line[x].transform.position.x < target.position.x)
+                {
+                    Line[x].transform.position += new Vector3(target.position.x,0.0f);
+                }      
+            }  
+        }
+    }
+}
